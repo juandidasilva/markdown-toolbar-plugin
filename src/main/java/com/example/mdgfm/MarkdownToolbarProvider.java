7b69cf620cf1f873e01b54229714cb3295d283cc
juandidasilva <44929949+juandidasilva@users.noreply.github.com>
2025-08-11 18:22:08 -0300

    feat: enable toolbar in GH diffs

--- src/main/java/com/example/mdgfm/MarkdownToolbarProvider.java
+++ src/main/java/com/example/mdgfm/MarkdownToolbarProvider.java
@@ -1,57 +1,33 @@
 package com.example.mdgfm;
 
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.toolbar.floating.AbstractFloatingToolbarProvider;
+import com.intellij.openapi.editor.toolbar.floating.FloatingToolbarComponent;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 /**
  * Provides floating toolbar with Markdown/GFM actions.
  */
 public class MarkdownToolbarProvider extends AbstractFloatingToolbarProvider {
 
-    private static final List<String> ACTION_IDS = List.of(
-            "MDGFM.Bold",
-            "MDGFM.Italic",
-            "MDGFM.InlineCode",
-            "MDGFM.Strike",
-            "MDGFM.CodeBlock",
-            "MDGFM.Heading",
-            "MDGFM.BulletList",
-            "MDGFM.OrderedList",
-            "MDGFM.Quote",
-            "MDGFM.TaskList",
-            "MDGFM.Suggestion",
-            "MDGFM.Table",
-            "MDGFM.Link",
-            "MDGFM.Mermaid",
-            "MDGFM.Details",
-            "MDGFM.DiffBlock",
-            "MDGFM.Mention",
-            "MDGFM.IssueRef",
-            "MDGFM.CommitRef",
-            "MDGFM.Emoji",
-            "MDGFM.Sup",
-            "MDGFM.Sub",
-            "MDGFM.Footnote",
-            "MDGFM.BatchAddSuggestion",
-            "MDGFM.BatchInsertSuggestions"
-    );
-
     public MarkdownToolbarProvider() {
         super("MDGFM.Toolbar");
     }
 
-    protected void registerActions(@NotNull List<? super AnAction> actions, @NotNull Editor editor) {
-        ActionManager am = ActionManager.getInstance();
-        ACTION_IDS.stream().map(am::getAction).forEach(actions::add);
+    @Override
+    public boolean isApplicable(@NotNull DataContext dataContext) {
+        Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+        return editor != null && editor.getDocument().isWritable();
     }
 
-    protected boolean isApplicable(@NotNull Editor editor) {
-        return editor.getDocument().isWritable();
+    @Override
+    public void register(@NotNull DataContext dataContext,
+                         @NotNull FloatingToolbarComponent component,
+                         @NotNull Disposable parentDisposable) {
+        // no dynamic actions to register
     }
 
     @Override
